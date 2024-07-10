db190386611b385e25f46d46b913db691d0f7647
Ranjith Kumar <123442191+S-RANJITH1@users.noreply.github.com>
2024-07-10 14:57:34 +0530

    complete

--- db.js
+++ db.js
@@ -3,11 +3,7 @@ import { MONGODB_URI } from "./config.js";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      serverSelectionTimeoutMS: 10000,
-    });
+    await mongoose.connect(MONGODB_URI);
     console.log("MongoDB Connected");
   } catch (error) {
     console.error("Error connecting to MongoDB:", error.message);
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import { MONGODB_URI, PORT } from "./config.js";
 import userRoutes from "./routes/userRoutes.js"; 
+import connectDB from "./db.js";
 
 const app = express();
 
@@ -10,21 +11,6 @@ app.use(express.urlencoded({ extended: true }));
 
 // Use the userRoutes router
 app.use("/api", userRoutes);
-
-// MongoDB connection
-const connectDB = async () => {
-  try {
-    await mongoose.connect(MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      serverSelectionTimeoutMS: 10000,
-    });
-    console.log("MongoDB Connected");
-  } catch (error) {
-    console.error("Error connecting to MongoDB:", error.message);
-    process.exit(1);
-  }
-};
 
 // Start server
 const startServer = async () => {
