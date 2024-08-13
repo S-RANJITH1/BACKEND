51b14a337f14ea04997f5d883539d28a41b2b1c6
Ranjith Kumar <123442191+S-RANJITH1@users.noreply.github.com>
2024-08-13 21:01:59 +0530

    change

--- config.js
+++ config.js
@@ -1,3 +1,5 @@
-export const MONGO_URI =
-  process.env.MONGO_URI || "mongodb://localhost:27017/mydatabase";
+import dotenv from 'dotenv';
+dotenv.config();
+
 export const PORT = process.env.PORT || 8000;
+export const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/mydatabase';
